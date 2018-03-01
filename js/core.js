d919d974d9d05c65be91e40d9e89785ae961e8e1
danbraj <danbraj@users.noreply.github.com>
2018-03-01 22:31:52 +0100

    sumowanie poprawnych odpowiedzi

--- js/core.js
+++ js/core.js
@@ -7,14 +7,29 @@ const colors = [
 
 const hTimer = document.querySelector('#timer');
 const hQuestion = document.querySelector('#question');
+const hAnswersList = document.querySelector('#answers-list');
+const hScore = document.querySelector('#score');
 
+let isAvailableAnswer = true;
 let timer = 5;
-setInterval(() => {
+const countdown = () => {
     if (timer == 0) {
         const index = Math.floor(Math.random() * 4);
         console.log(index, colors[index]);
         hQuestion.style.backgroundColor = colors[index];
+        isAvailableAnswer = true;
         timer = 5;
     } 
     hTimer.textContent = timer--;
-}, 1000);
+}
+
+let score = 0;
+hAnswersList.addEventListener('click', function(evt) {
+    if (isAvailableAnswer && evt.target.attributes['data-name'].textContent === hQuestion.style.backgroundColor) {
+        isAvailableAnswer = false;
+        hScore.textContent = ++score;
+        timer = 0;
+    }
+});
+
+setInterval(countdown, 1000);
