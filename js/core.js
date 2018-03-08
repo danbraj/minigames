73703e6e9b494cd31a13ed3cdbd1b5513ade45f5
danbraj <danbraj@users.noreply.github.com>
2018-03-08 11:40:46 +0100

    dodanie frameworka Vue

--- js/core.js
+++ js/core.js
@@ -1,35 +1,43 @@
-const colors = [
-    'red',
-    'green',
-    'blue',
-    'yellow'
-];
-
-const hTimer = document.querySelector('#timer');
-const hQuestion = document.querySelector('#question');
-const hAnswersList = document.querySelector('#answers-list');
-const hScore = document.querySelector('#score');
-
-let isAvailableAnswer = true;
-let timer = 5;
-const countdown = () => {
-    if (timer == 0) {
-        const index = Math.floor(Math.random() * 4);
-        console.log(index, colors[index]);
-        hQuestion.style.backgroundColor = colors[index];
-        isAvailableAnswer = true;
-        timer = 5;
-    } 
-    hTimer.textContent = timer--;
-}
-
-let score = 0;
-hAnswersList.addEventListener('click', function(evt) {
-    if (isAvailableAnswer && evt.target.attributes['data-name'].textContent === hQuestion.style.backgroundColor) {
-        isAvailableAnswer = false;
-        hScore.textContent = ++score;
-        timer = 0;
+var game = new Vue({
+    el: '#game',
+    data: {
+        intervalId: null,
+        taskCounter: 0,
+        timer: 5,
+        score: 0,
+        isAvailableAnswer: true,
+        isCorrectAnswer: false,
+        currColor: 'white',
+        colors: [
+            'red',
+            'green',
+            'blue',
+            'yellow'
+        ]
+    },
+    methods: {
+        countdown: function () {
+            this.timer--;
+            if (this.isCorrectAnswer || this.timer == 0) {
+                this.isCorrectAnswer = false;
+                const index = Math.floor(Math.random() * 4);
+                // console.log(index, this.colors[index]);
+                this.currColor = this.colors[index];
+                this.taskCounter++;
+                this.isAvailableAnswer = true;
+                this.timer = 5;
+            }
+        },
+        answer: function (evt) {
+            // console.log(evt.target);
+            if (this.isAvailableAnswer && evt.target.attributes['data-name'].textContent === this.currColor) {
+                this.isAvailableAnswer = false;
+                this.score++;
+                this.isCorrectAnswer = true;
+            }
+        }
+    },
+    mounted: function () {
+        this.intervalId = setInterval(this.countdown, 1000);
     }
 });
-
-setInterval(countdown, 1000);
