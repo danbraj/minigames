ac4b68e8a2a5eab5a2d6afc91f889e06119102df
danbraj <danbraj@users.noreply.github.com>
2018-08-09 13:13:00 +0200

    Prepare main functionalities part 1
    * Change grid display to absolute position
    * Remove and fill items, but w/o animations

--- src/pages/Match3.vue
+++ src/pages/Match3.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h2>Match 3</h2>
         <div class="board" v-bind:class="{ loaded }">
-            <item v-for="(item, index) in items" :key="index" :item="item"></item>
+            <item v-for="(e, idx) in items" :key="idx" :item="e" v-if="e != null" v-bind:style="{ left: px(e.x*100), top: px(e.y*100) }" @click.native="del(idx)"></item>
         </div>
     </div>
 </template>
@@ -21,27 +21,65 @@ let itemsDefs = [
   'grape'
 ];
 
+const itemsTypes = itemsDefs.sort(() => 0.5 - Math.random()).slice(0, 5);
+const count = itemsTypes.length;
+
 export default {
     components: {
         item: Item
     },
     created() {
-        const itemsTypes = itemsDefs.sort(() => 0.5 - Math.random()).slice(0, 5);
-        const count = itemsTypes.length;
         let arr = [];
+        let x = 0, y = 0;
         for (let i = 0; i < 80; i++) {
-            const idx = Math.floor(Math.random() * count);
-            arr.push(itemsTypes[idx]);
+            const idx = Math.floor(Math.random() * count); 
+            arr.push({
+                handle: itemsTypes[idx],
+                x,
+                y
+            });
+            x++;
+            if (i % 10 == 9) {
+                y++;
+                x = 0;
+            }
         }
-        this.items = arr.map(x => {
-            return { handle: x };
-        });
+        this.items = arr;
         this.loaded = true;
     },
     data() {
         return {
             items: [],
             loaded: false
+        }
+    },
+    methods: {
+        del(idx) {
+            console.log(idx);
+            const x = idx % 10;             // this.items[idx].x
+            const y = Math.floor(idx / 10); // this.items[idx].y
+
+            // console.log(x, y);
+            let prev;
+            for (let i = y; i >= 0; i--) {
+                
+                const index = x + i * 10;
+                if (i < y) {
+                    this.items[prev] = this.items[index];
+                    this.items[prev].y++;
+                } 
+                if (i == 0) {
+                    this.$set(this.items, index, {
+                        handle: itemsTypes[Math.floor(Math.random() * count)],
+                        x: index,
+                        y: 0
+                    });
+                };
+                prev = index;
+            }
+        },
+        px(value) {
+            return `${value}px`;
         }
     }
 }
@@ -53,22 +91,22 @@ export default {
   margin: 0 auto;
 }
 .board {
-    display: grid;
-    grid-template: repeat(8, 1fr) / repeat(10, 1fr);
+    position: relative;
     width: 1000px;
     height: 800px;
     overflow: hidden;
 }
 .item {
-    position: relative;
-    top: -800px;
-    .loaded > & {
-        background-color: white;
-        animation: godown 2s normal forwards cubic-bezier(0.550, 0.055, 0.675, 0.190);
-        @keyframes godown {
-            from {top: -800px}
-            to {top: 0}
-        }
-    }
+    position: absolute;
+    top: -100px;
+    transition: top 2s ease;
+    // .loaded > & {
+    //     background-color: white;
+    //     animation: godown 2s normal forwards cubic-bezier(0.550, 0.055, 0.675, 0.190);
+    //     @keyframes godown {
+    //         from {top: -800px}
+    //         to {top: 0}
+    //     }
+    // }
 }
 </style>
